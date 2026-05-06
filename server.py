5a7227c1d29cdef63a6a0401d3fe54dd3c4be063
Ahmad-Jamous <ahmad.jamous@outlook.de>
2026-05-06 14:45:12 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""This Module controls the application Emotion Detector"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,9 +6,10 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """This function detects the emotion based on AI and given test of a user"""
     text_to_analyze = request.args.get('textToAnalyze')
     result =  emotion_detector(text_to_analyze)
-    string_to_return = f'''For the given statement, the system response is 
+    string_to_return = f'''For the given statement, the system response is
     'anger': {result['anger']}, 
     'disgust': {result['disgust']}, 
     'fear': {result['fear']}, 
@@ -15,9 +17,12 @@ def detect_emotion():
     and 'sadness': {result['sadness']}. 
     The dominant emotion is {result['dominant_emotion']}.    
     '''
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     return string_to_return
 @app.route("/")
 def show_homepage():
+    """This function renders the homepage"""
     return render_template("index.html")
 if __name__ == "__main__":
     app.run("0.0.0.0", port = 5000)
