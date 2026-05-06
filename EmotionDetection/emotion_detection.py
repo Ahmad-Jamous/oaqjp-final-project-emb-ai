e2233c803a3226bfa0b96492ad292852a4ce126b
Ahmad-Jamous <ahmad.jamous@outlook.de>
2026-05-06 14:45:45 +0200

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,9 @@ def emotion_detector(text_to_analyze):
     json_text = { "raw_document": { "text": text_to_analyze } } 
 
     response = requests.post(url, json = json_text, headers = header)
+    if response.status_code == 400:
+        return {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+
     formatted_response = json.loads(response.text)
     
     emotions = formatted_response['emotionPredictions'][0]['emotion']
